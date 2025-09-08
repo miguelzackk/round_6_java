7b4bfc5fd25d8eaaae6bc884c6980d1bceb093c4
miguelzackk <mzsilvaw@gmail.com>
2025-09-08 11:05:13 -0300

    start vio

---
+++ src/round_6_java/Jogador.java
@@ -0,0 +1,21 @@
+package round_6_java;
+
+public class Jogador {
+	private int numJogador;
+	private boolean status = true;
+	public int getNumJogador() {
+		return numJogador;
+	}
+	public void setNumJogador(int numJogador) {
+		this.numJogador = numJogador;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	
+	
+}
--- src/round_6_java/Main.java
+++ src/round_6_java/Main.java
@@ -1,8 +1,11 @@
 package round_6_java;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        Lider lider = new Lider();
-        lider.iniciarJogo();
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Vip vip = new Vip();
+		vip.VipJogo(sc);
+	}
 }
