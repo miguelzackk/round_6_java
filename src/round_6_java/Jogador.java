0894168f8d5f8696a8a4cda504e3c9f7c437dfd9
miguelzackk <mzsilvaw@gmail.com>
2025-09-08 21:56:38 -0300

    feat: finish vip.

--- src/round_6_java/Jogador.java
+++ src/round_6_java/Jogador.java
@@ -1,21 +1,22 @@
 package round_6_java;
 
 public class Jogador {
-	private int numJogador;
-	private boolean status = true;
-	public int getNumJogador() {
-		return numJogador;
-	}
-	public void setNumJogador(int numJogador) {
-		this.numJogador = numJogador;
-	}
-	public boolean isStatus() {
-		return status;
-	}
-	public void setStatus(boolean status) {
-		this.status = status;
-	}
-	
-	
-	
+    private int numJogador;
+    private boolean status = true;
+
+    public int getNumJogador() {
+        return numJogador;
+    }
+
+    public void setNumJogador(int numJogador) {
+        this.numJogador = numJogador;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean statusVivo) {
+        this.status = statusVivo;
+    }
 }
